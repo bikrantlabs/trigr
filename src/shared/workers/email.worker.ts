bb2c1b931deef30a98ce92b636022c10b5d1541f
BikrantJ <bikrantjung1214@gmail.com>
2026-05-09 18:22:49 +0545

    feat(infra): setup email delivery with mailpit

--- src/shared/workers/email.worker.ts
+++ src/shared/workers/email.worker.ts
@@ -1,20 +1,16 @@
+import { sendAccountVerficationEmail } from "../emails/account-verification";
+import { sendWelcomeEmail } from "../emails/welcome";
 import { createWorker } from "../infra/events/worker";
 import { logger } from "../logger";
 
 export function startEmailWorker() {
   return createWorker("auth_email", {
     "send-verification-email": async (payload) => {
-      // TODO: Send email.
-      await new Promise((resolve) => {
-        setTimeout(resolve, 1000);
-      });
+      await sendAccountVerficationEmail(payload.email, payload.code);
       logger.info({ email: payload.email }, "Verification email sent");
     },
     "send-welcome-email": async (payload) => {
-      // TODO: Send email.
-      await new Promise((resolve) => {
-        setTimeout(resolve, 1000);
-      });
+      await sendWelcomeEmail(payload.email);
       logger.info({ email: payload.email }, "Welcome email sent");
     },
   });
